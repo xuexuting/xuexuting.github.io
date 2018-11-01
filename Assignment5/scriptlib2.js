18ab82adab5317469da1f05772e0066d2c76841f
xuexuting <xuexuting960208@163.com>
2018-11-01 20:40:01 +0800

    Uplpad

--- Assignment5/scriptlib2.js
+++ Assignment5/scriptlib2.js
@@ -1,138 +1,40 @@
-$(document).ready(function(){
-
-    $("button#hide_h2").click(function() {
-        $("h2").hide(500);
-    });
-
-    $("button#show_h2").click(function() {
-        $("h2").show(300);
-        $("h2").css("color","blue");
-        $("h2").html("You clicked me hard.");
-    });
-
-    $("button#clear_screen").click(function() {
-        var $x = $("container");
-        $x.empty();
-    });
-
-    $("button#get_data").click(function() {
-        var items = [];
-        var i = 0;
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
-        var dataSet = [];
-        $.getJSON(airtable_read_endpoint, function(result) {
-               $.each(result.records, function(key,value) {
-                   items = [];
-                       items.push(value.fields.Name);
-                       items.push(value.fields.Stage_Desc);
-                       items.push(value.fields.Completed);
-                       items.push(value.fields.Time_Estimate);
-                       items.push(value.fields.Weight_Factor);
-                       items.push(value.fields.converted);
-                       dataSet.push(items);
-                       console.log(items);
-                }); // end .each
-                console.log(dataSet);
-
-             $('#example').DataTable( {
-                 data: dataSet,
-                 retrieve: true,
-                 columns: [
-                     { title: "Name",
-                       defaultContent:""},
-                     { title: "Stage",
-                         defaultContent:"" },
-                     { title: "Completed",
-                       defaultContent:"" },
-                     { title: "Time Estimated",
-                       defaultContent:""},
-                     { title: "Weight Factor",
-                         defaultContent:""},
-                     { title: "Converted",
-                       defaultContent:""},
-                 ]
-             } );
-        }); // end .getJSON
-     }); // end button
-
-     $("button#roll_up").click(function() {
-       var table1_items = [];
+$("button#get_data").click(function() {
+       var items = [];
        var i = 0;
-       var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
-       var table1_dataSet = [];
+       var airtable_read_endpoint = "https://api.airtable.com/v0/appAgox3WPwODsSrr/Plant?api_key=keyBadmTVmE3SwXQR";
+       var dataSet = [];
        $.getJSON(airtable_read_endpoint, function(result) {
               $.each(result.records, function(key,value) {
-                  table1_items = [];
-                      table1_items.push(value.fields.Name);
-                      table1_items.push(value.fields.Stage_Desc);
-                      table1_items.push(value.fields.Completed);
-                      table1_items.push(value.fields.Time_Estimate);
-                      table1_items.push(value.fields.Weight_Factor);
-                      table1_items.push(value.fields.converted);
-                      table1_dataSet.push(table1_items);
-                      console.log(table1_items);
+                  items = [];
+                      items.push(value.fields.Plant_Name);
+                      items.push(value.fields.Every_Plant_Size);
+                      items.push(value.fields.Plant_Price_copy);
+                      items.push(value.fields.Difficulty_Copy);
+                      items.push(value.fields.Light_Level_Copy);
+                      items.push(value.fields.Pet_Friendly_Copy);
+                        items.push(value.fields.Discription);
+                      dataSet.push(items);
                }); // end .each
-               console.log(table1_dataSet);
-
-            $('#table1').DataTable( {
-                data: table1_dataSet,
+            $("#example").DataTable( {
+                data: dataSet,
                 retrieve: true,
                 columns: [
-                    { title: "Name",
+                    { title: "Plant Name",
                       defaultContent:""},
-                    { title: "Stage",
-                        defaultContent:"" },
-                    { title: "Completed",
+                    { title: "Every Plant Size",
                       defaultContent:"" },
-                    { title: "Time Estimated",
+                    { title: "Plant Price",
                       defaultContent:""},
-                    { title: "Weight Factor",
+                      { title: "Difficulty",
                         defaultContent:""},
-                    { title: "Converted",
-                      defaultContent:""},
-                ]
-            } );
-       }); // end .getJSON
+                        { title: "Light Level",
+                            defaultContent:""},
+                            { title: "Pet Friendly",
+                                defaultContent:""},
+                                { title: "Discription",
+                                    defaultContent:""},
 
-         var table2_items = [];
-         var i = 0;
-         var airtable_read_endpoint =
-         "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Stage?api_key=keyTcsTzckqyBTlk8&view=Grid%20view";
-         var table2_dataSet = [];
-         $.getJSON(airtable_read_endpoint, function(result) {
-                $.each(result.records, function(key,value) {
-                    table2_items = [];
-                        table2_items.push(value.fields.Name);
-                        table2_items.push(value.fields.Total_Entries);
-                        table2_dataSet.push(table2_items);
-                        console.log(table2_items);
-                 }); // end .each
-                 console.log(table2_dataSet);
-                $('#table2').DataTable( {
-                    data: table2_dataSet,
-                    retrieve: true,
-                    ordering: false,
-                    columns: [
-                        { title: "Name",
-                          defaultContent:""},
-                        { title: "Total Entries",
-                          defaultContent:""},
-                    ] // rmf columns
-                } ); // end dataTable
-
-                var chart = c3.generate({
-                     data: {
-                         columns: table2_dataSet,
-                         type : 'bar'
-                     },
-                     donut: {
-                         title: "Tasks for Each Stage:",
-                     }
-                 });
-
-          }); // end .getJSON
-       }); // end button
-
-        // $.getJSON('http://localhost/d756a/data_export.json/Computer+TV', function(obj) {
-
-}); // document ready
+                  ]
+                              } );
+                         }); // end .getJSON
+                      }); // end button
