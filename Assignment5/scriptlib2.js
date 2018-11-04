5ba79167ece1fd450c380af4873812b7fe687f26
xuexuting <xuexuting960208@163.com>
2018-11-04 18:51:03 +0800

    Upload

--- Assignment5/scriptlib2.js
+++ Assignment5/scriptlib2.js
@@ -7,13 +7,14 @@ $("button#get_data").click(function() {
               $.each(result.records, function(key,value) {
                   items = [];
                       items.push(value.fields.Plant_Name);
+                      items.push('<td>< img src="' + value.fields.Images + '" style="width: 350px;"/></td>');
                       items.push(value.fields.Every_Plant_Size);
                       items.push(value.fields.Plant_Price_copy);
                       items.push(value.fields.Difficulty_Copy);
                       items.push(value.fields.Light_Level_Copy);
                       items.push(value.fields.Pet_Friendly_Copy);
                         items.push(value.fields.Discription);
-                          items.push(value.fields.Images);
+
                       dataSet.push(items);
                }); // end .each
 
@@ -23,6 +24,8 @@ $("button#get_data").click(function() {
                 columns: [
                     { title: "Plant Name",
                       defaultContent:""},
+                      { title: "Plant Images",
+                        defaultContent:""},
                     { title: "Every Plant Size",
                       defaultContent:"" },
                     { title: "Plant Price",
@@ -34,47 +37,9 @@ $("button#get_data").click(function() {
                             { title: "Pet Friendly",
                                 defaultContent:""},
                                 { title: "Discription",
-                                    defaultContent:""},
-                                    { title: "Images",
-                                        defaultContent:""},
+                                    defaultContent:""}
 
-                  ]
-                              } );
-
-                              var table2_items = [];
-         var i = 0;
-         var airtable_read_endpoint =
-         "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Stage?api_key=keyTcsTzckqyBTlk8&view=Grid%20view";
-         var table2_dataSet = [];
-         $.getJSON(airtable_read_endpoint, function(result) {
-                $.each(result.records, function(key,value) {
-                    table2_items = [];
-                        table2_items.push(value.fields.Name);
-                        table2_items.push(value.fields.Total_Entries);
-                        table2_dataSet.push(table2_items);
-                        console.log(table2_items);
-                 }); // end .each
-                 console.log(table2_dataSet);
-                $('#table2').DataTable( {
-                    data: table2_dataSet,
-                    retrieve: true,
-                    ordering: false,
-                    columns: [
-                        { title: "Name",
-                          defaultContent:""},
-                        { title: "Total Entries",
-                          defaultContent:""},
-                    ] // rmf columns
-                } ); // end dataTable
-                
-                              var chart = c3.generate({
-                     data: {
-                         columns: table2_dataSet,
-                         type : 'bar'
-                     },
-                     bar: {
-                         title: "Tasks for Each Stage:",
-                     }
-                 });
-                         }); // end .getJSON
-                      }); // end button
+                                  ]
+                                              } );
+                                         }); // end .getJSON
+                                      }); // end button
