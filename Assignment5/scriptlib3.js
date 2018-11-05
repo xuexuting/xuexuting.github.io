ecbbb3740df29e480111f5bf2e73022ff39d7542
xuexuting <xuexuting960208@163.com>
2018-11-05 12:59:23 +0800

    Update

---
+++ Assignment5/scriptlib3.js
@@ -0,0 +1,64 @@
+
+                                      $("button#get_data").click(function() {
+                                             var items = [];
+                                             var i = 0;
+                                             var airtable_read_endpoint = "https://api.airtable.com/v0/appAgox3WPwODsSrr/Plant?api_key=keyBadmTVmE3SwXQR";
+                                             var dataSet = [];
+                                             $.getJSON(airtable_read_endpoint, function(result) {
+                                                    $.each(result.records, function(key,value) {
+                                                        items = [];
+                                                            items.push(value.fields.Plant_Name);
+                                                              items.push('<td><img src="' + value.fields.Images + '" style="width: 150px;"/></td>');
+                                                            items.push(value.fields.Plant_Size);
+                                                            items.push(value.fields.Plant_Price_copy);
+                                                            items.push(value.fields.Difficulty_Copy);
+                                                            items.push(value.fields.Light_Level_Copy);
+                                                            items.push(value.fields.Pet_Friendly_Copy);
+                                                              items.push(value.fields.Discription);
+                                                            dataSet.push(items);
+                                                     }); // end .each
+                                                  $("#example").DataTable( {
+                                                      data: dataSet,
+                                                      retrieve: true,
+                                                      columns: [
+                                                          { title: "Plant Name",
+                                                            defaultContent:""},
+                                                            { title: "Plant Images",
+                                                              defaultContent:""},
+                                                          { title: "Plant Size",
+                                                            defaultContent:"" },
+                                                          { title: "Plant Price",
+                                                            defaultContent:""},
+                                                            { title: "Difficulty",
+                                                              defaultContent:""},
+                                                              { title: "Light Level",
+                                                                  defaultContent:""},
+                                                                  { title: "Pet Friendly",
+                                                                      defaultContent:""},
+                                                                      { title: "Discription",
+                                                                          defaultContent:""},
+
+                                                        ]
+                                                                    } );
+                                                               }); // end .getJSON
+
+                                                               var chart = c3.generate({
+                                                                   data: {
+                                                                       columns: [
+                                                                           ['sample', 30, 200, 100, 400, 150, 250],
+                                                                           ['sample2', 130, 300, 200, 500, 250, 350]
+                                                                       ],
+
+                                                                   },
+                                                                   axis: {
+                                                                       x: {
+                                                                           label: 'X Label'
+                                                                       },
+                                                                       y: {
+                                                                           label: 'Y Label'
+                                                                       }
+                                                                   }
+                                                               });
+
+
+                                                            }); // end button
