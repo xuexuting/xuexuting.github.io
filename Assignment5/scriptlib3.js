cade332f8357d2ad5ca24205459f7ebda1a8256e
xuexuting <xuexuting960208@163.com>
2018-11-05 14:12:04 +0800

    Update Assignment5/scriptlib3.js

--- Assignment5/scriptlib3.js
+++ Assignment5/scriptlib3.js
@@ -59,15 +59,13 @@
                                                          bar: {
                                                              width: {
                                                                  ratio: 0.5 // this makes bar width 50% of length between ticks
-                                                             }
+                                                             },
                                                              // or
                                                              //width: 100 // this makes bar width 100px
-                                                             color: {
+                                                         color: {
                                                            pattern: ['#b6edb1', '#64aa5d', '#2d7227', '#0d5407',]
                                                          }
                                                      });
-
-                                              }); // end .getJSON
 
 
                                           }); // end button
