2961459d45e261f7a9e88ac7be5c9fea1935c42c
xuexuting <xuexuting960208@163.com>
2018-11-05 14:23:30 +0800

    Update Assignment5/scriptlib4.js

--- Assignment5/scriptlib4.js
+++ Assignment5/scriptlib4.js
@@ -45,17 +45,23 @@
                                                                var chart = c3.generate({
                                                                    data: {
                                                                        columns: [
-                                                                           ['data1', 30, 200, 100, 400, 150, 250],
-                                                                           ['data2', 130, 100, 140, 200, 150, 50]
+                                                                         ['small', 7],
+                                                                         ['medium', 9],
+                                                                          ['large', 6],
+                                                                           ['xlarge', 6]
                                                                        ],
                                                                        type: 'bar'
                                                                    },
                                                                    bar: {
                                                                        width: {
                                                                            ratio: 0.5 // this makes bar width 50% of length between ticks
-                                                                       }
+                                                                       },
                                                                        // or
                                                                        //width: 100 // this makes bar width 100px
+                                                                   color: {
+                                                                         pattern: ['#b6edb1', '#64aa5d', '#2d7227', '#0d5407',]
+                                                                       },
+
                                                                    }
                                                                });
 
